919a3e5c5c8529fedad4d77e5d22b36ac0c453d5
Denise Case <dcase@nwmissouri.edu>
2019-03-22 08:16:20 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Vue Course Planner (example)
+
+- Source
+- Website
+
+## References
+
+<https://codepen.io/nikolasp/pen/yvpWJR>
---
+++ main.js
@@ -0,0 +1,39 @@
+Vue.use(VueDraggable);
+
+new Vue({
+  data: {
+    options: {
+      // dropzoneSelector: 'ul',
+      // draggableSelector: 'li',
+      // excludeOlderBrowsers: true,
+      // showDropzoneAreas: true,
+      // multipleDropzonesItemsDraggingEnabled: true,
+      // onDrop(event) {},
+      // onDragstart(event) {
+      //   event.stop();
+      // },
+      // onDragend(event) {
+      //   event.stop();
+      // }
+    }
+  },
+  template: `
+  <div v-drag-and-drop:options="options" class="drag-wrapper">
+    <ul>
+      <li>Item 1</li>
+      <li>Item 2</li>
+      <li>Item 3</li>
+    </ul>
+    <ul>
+      <li>Item 4</li>
+      <li>Item 5</li>
+      <li>Item 6</li>
+    </ul>
+    <ul>
+      <li>Item 7</li>
+      <li>Item 8</li>
+      <li>Item 9</li>
+    </ul>
+  </div>
+  `
+}).$mount("#app");
